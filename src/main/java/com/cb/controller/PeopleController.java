8a7a613529e7a4521f6ac4d0ebe071ca216b40f1
root <3464869606@qq.com>
2022-07-13 14:11:38 +0800

    第三次提交 增加了无参构造器

--- src/main/java/com/cb/controller/PeopleController.java
+++ src/main/java/com/cb/controller/PeopleController.java
@@ -5,4 +5,9 @@ public class PeopleController {
     public static void main(String[] args) {
         System.out.println("这是一个程序!!!");
     }
+
+    public void PeopleController()
+    {
+        System.out.println("无参构造器");
+    }
 }
